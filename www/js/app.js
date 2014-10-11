6618f06112d39a635ed5aa2bcc319860eb295334
Titus Woo <titus.woo@me.com>
2014-10-10 20:42:16 -0400

    grabs weather for current location and displays it

--- www/js/app.js
+++ www/js/app.js
@@ -5,15 +5,74 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .controller('weatherCtrl', function ($scope, $http, $q) {
+        var BASE_URL = 'https://query.yahooapis.com/v1/public/yql';
+        $scope.weather = {};
+
+        $scope.getLocation = function () {
+            var deferred = $q.defer();
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    deferred.resolve(position);
+                });
+            } else {
+                return deferred.reject();
+            }
+            return deferred.promise;
+        };
+
+        $scope.getWoeid = function (latitude, longitude) {
+            return $http.get(BASE_URL, {
+                params: {
+                    q: 'select * from geo.placefinder where text="' +
+                        latitude + ', ' + longitude + '" and gflags="R"',
+                    format: 'json'
+                }
+            });
+        };
+
+        $scope.getWeather = function (woeid) {
+            return $http.get(BASE_URL, {
+                params: {
+                    q: 'select * from weather.forecast where woeid="' + woeid + '"',
+                    format: 'json'
+                }
+            });
+        };
+
+        $scope.refresh = function () {
+            $scope.getLocation()
+                .then(function (position) {
+                    $scope.position = position;
+                    var pos = position.coords;
+                    return $scope.getWoeid(pos.latitude, pos.longitude);
+                })
+                .then(function (data) {
+                    var woeid = data.data.query.results.Result.woeid;
+                    return $scope.getWeather(woeid);
+                })
+                .then(function (weather) {
+                    if (weather.status === 200) {
+                        var channel = weather.data.query.results.channel;
+                        $scope.weather = channel;
+                    } else {
+                        console.log('error getting weather information.');
+                    }
+                });
+        };
+
+        $scope.refresh();
+    });
